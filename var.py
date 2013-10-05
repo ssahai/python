02c7e3824cc9d9e5ccc120ccd3909b2682aa3b15
Shubham Sahai <ssahai@cse.iitk.ac.in>
2013-10-05 07:26:08 +0530

    script demonstrating use of variables

---
+++ var.py
@@ -0,0 +1,13 @@
+i = 5
+print i
+i = i+3
+print i
+
+s = '''This is a multiline string.
+The second line is this.'''
+print s
+
+t = "This is another multiline string.\
+Hello from second line."
+
+print t
